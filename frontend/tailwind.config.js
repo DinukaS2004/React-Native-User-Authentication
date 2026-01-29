539a683a0c35085c97b8a92d7e760fe2aa238775
Dinuka Samarasinghe <dinukad735@gmail.com>
2026-01-29 22:52:09 +0530

    Initialized the backend

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,14 @@ module.exports = {
   ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        regular: ['Inter-Regular', 'sans-serif'],
+        medium: ['Inter-Medium', 'sans-serif'],
+        semibold: ['Inter-SemiBold', 'sans-serif'],
+        bold2: ['Bebas Neue Regular'],
+      },
+    },
   },
   plugins: [],
 }
